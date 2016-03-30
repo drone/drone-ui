916bde832890e2489ae7068c5986da3c8c5099f8
Matthias Loibl <mail@matthiasloibl.com>
2016-03-31 01:46:16 +0200

    Create style.js with global styles object

---
+++ src/style.js
@@ -0,0 +1,23 @@
+export
+const styles = {
+  layout: {
+    maxWidth: 1024,
+    margins: {
+      top: 16,
+      right: 32,
+      bottom: 16,
+      left: 32
+    }
+  },
+  input: {
+    padding: '0.3rem 0.75rem',
+    border: 'none',
+    color: '#747C84',
+    backgroundColor: '#eff1f5'
+  },
+  hr: {
+    height: 1,
+    backgroundColor: '#eee',
+    border: 'none'
+  }
+};
