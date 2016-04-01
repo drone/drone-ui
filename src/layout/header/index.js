e5196a209d805a4bab7ec039fed7df695d1022bc
Matthias Loibl <mail@matthiasloibl.com>
2016-04-02 00:45:34 +0200

    Link to / with logo in the header

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import Row from '../grid/row';
 import Col from '../grid/col';
@@ -14,7 +15,9 @@ class Header extends React.Component {
       <header className="header">
         <Row>
           <Col xs={6} sm={1}>
-            <Logo/>
+            <Link to="/">
+              <Logo/>
+            </Link>
           </Col>
           <Col xs={6} sm={2} last-sm>
             <Dropdown/>
