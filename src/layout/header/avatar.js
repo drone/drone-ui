e942b8b0b2efb7089a3c8fe445161de66fdda6bb
Matthias Loibl <mail@matthiasloibl.com>
2016-04-02 13:25:37 +0200

    Avatar gets avatar_url as src

--- src/layout/header/avatar.js
+++ src/layout/header/avatar.js
@@ -3,8 +3,9 @@ import React from 'react';
 export default
 class Avatar extends React.Component {
   render() {
+    const {src} = this.props;
     return (
-      <img className="avatar" src="https://secure.gravatar.com/avatar/915d80f0d9b6678fad4d1ab36dfc8960" />
+      <img className="avatar" src={src}/>
     );
   }
 }
