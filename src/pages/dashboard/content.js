d86c08e292dcf2691b03ecadd8ac38fafad6c698
Matthias Loibl <mail@matthiasloibl.com>
2016-04-06 14:20:33 +0200

    Create dashboard page

---
+++ src/pages/dashboard/content.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { getUserRepos } from '../../data/repos/actions';
+import RepoListItem from '../../components/repo_list_item';
+
+class Content extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(getUserRepos());
+  }
+
+  render() {
+    let {repos} = this.props;
+
+    if (repos.size == 0) {
+      return <div></div>;
+    }
+
+    // Sort by repo's name
+    repos = repos.toList();
+    repos = repos.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+
+    return (
+      <div className="dashboard">
+        {repos.map((repo, index) => {
+          return (
+            <div>
+              <RepoListItem key={repo.id} repo={repo}/>
+              {index < repos.size - 1 ? <hr/> : null}
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    repos: state.drone.repos
+  })
+)(Content)
---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,7 @@
+import Header from './header';
+import Content from './content';
+
+export default {
+  Header,
+  Content
+}
