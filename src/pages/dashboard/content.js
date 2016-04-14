ea4a41ffcf703003439f3c2791a3b9156ed4a014
Matthias Loibl <mail@matthiasloibl.com>
2016-04-14 02:58:51 +0200

    Add filtering repositories on dashboard

--- src/pages/dashboard/content.js
+++ src/pages/dashboard/content.js
@@ -6,6 +6,16 @@ import { getUserRepositories } from '../../data/repositories/actions';
 import RepoListItem from '../../components/repo_list_item';
 
 class Content extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filter: ''
+    };
+
+    this.onFilter = this.onFilter.bind(this);
+  }
+
   componentDidMount() {
     this.props.dispatch(getUserRepositories());
   }
@@ -17,14 +27,18 @@ class Content extends React.Component {
       return <div></div>;
     }
 
-    // Sort by repo's name
-    repositories = repositories.toList();
-    repositories = repositories.sort((a, b) => {
-      return a.get('name').localeCompare(b.get('name'));
-    });
+    repositories = repositories.toList()
+      .filter((repository) => { // filter repositories for names that match the filtered name
+        return repository.get('name').indexOf(this.state.filter) > -1;
+      })
+      .sort((a, b) => { // sort repositories by name ascending
+        return a.get('name').localeCompare(b.get('name'));
+      });
 
     return (
       <div className="dashboard">
+        <input type="search" placeholder="Filter..." onChange={this.onFilter}/>
+
         {repositories.map((repo, index) => {
           return (
             <Link key={repo.get('id')} to={`/${repo.get('owner')}/${repo.get('name')}`}>
@@ -35,6 +49,12 @@ class Content extends React.Component {
         })}
       </div>
     );
+  }
+
+  onFilter(event) {
+    this.setState({
+      filter: event.target.value
+    });
   }
 }
 
