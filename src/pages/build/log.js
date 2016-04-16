ef2d21f04737a22550a9f83e82cffed2e88b473c
Matthias Loibl <mail@matthiasloibl.com>
2016-04-17 01:52:20 +0200

    If build is running start EventSource, http get otherwise

--- src/pages/build/log.js
+++ src/pages/build/log.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import Request from 'superagent';
 
+import { RUNNING } from '../../components/status';
+
 export default
 class Log extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      text: null
+      text: ''
     };
   }
 
   componentDidMount() {
     const {owner, name, build, job} = this.props;
 
-    Request.get(`/api/repos/${owner}/${name}/logs/${build}/${job}`)
+    if (job.get('status') == RUNNING) {
+      this.requestStream(owner, name, build, job);
+    } else {
+      this.requestLog(owner, name, build, job);
+    }
+  }
+
+  requestLog(owner, name, build, job) {
+    Request.get(`/api/repos/${owner}/${name}/logs/${build.get('number')}/${job.get('number')}`)
       .end((err, response) => {
         if (err != null) {
           console.error(err); // TODO: Add ui error handling
@@ -24,6 +34,23 @@ class Log extends React.Component {
           text: response.text
         });
       });
+  }
+
+  requestStream(owner, name, build, job) {
+    const events = new EventSource(
+      `/api/stream/${owner}/${name}/${build.get('number')}/${job.get('number')}`,
+      {withCredentials: true}
+    );
+
+    events.onmessage = (event) => {
+      this.setState({
+        text: this.state.text + event.data
+      });
+    };
+
+    events.onerror = (event) => {
+      console.log('user event stream closed due to error.', event); // TODO: Create UI feedback for error
+    };
   }
 
   render() {
