db7adc2e1c7d79123b9572a7a8900398d5ea56fa
Matthias Loibl <mail@matthiasloibl.com>
2016-04-25 19:37:40 +0200

    Add createUser(), updateUser() & deleteUser() actions for users

--- src/data/users/actions.js
+++ src/data/users/actions.js
@@ -7,14 +7,14 @@ export const userSchema = new Schema('user');
 export function fetchWindowUser() {
   return dispatch => {
     const stateFromServer = normalize(window.STATE_FROM_SERVER.user, userSchema);
-    dispatch(userListUpdate(Immutable.fromJS(stateFromServer.entities.user)));
+    dispatch(setUser(Immutable.fromJS(stateFromServer.entities.user)));
   };
 }
 
-export const USER_UPDATE = 'USER_UPDATE';
-export function userListUpdate(user) {
+export const USER_SET = 'USER_SET';
+export function setUser(user) {
   return {
-    type: USER_UPDATE,
+    type: USER_SET,
     user
   };
 }
@@ -27,8 +27,8 @@ export function getUsers() {
           console.error(err); // TODO: Add ui error handling
         }
 
-        const users = normalize(JSON.parse(response.text), arrayOf(userSchema));
-        dispatch(usersReceived(Immutable.fromJS(users.entities.user)));
+        response = normalize(JSON.parse(response.text), arrayOf(userSchema));
+        dispatch(usersReceived(Immutable.fromJS(response.entities.user)));
       });
   };
 }
@@ -38,5 +38,72 @@ export function usersReceived(users) {
   return {
     type: USERS_RECEIVED,
     users
+  };
+}
+
+export function createUser(login) {
+  return dispatch => {
+    Request.post('/api/users/')
+      .send({login})
+      .end((err, response) => {
+        if (err != null) {
+          console.error(err); // TODO: Add ui error handling
+        }
+
+        response = normalize(JSON.parse(response.text), userSchema);
+        dispatch(userCreated(Immutable.fromJS(response.entities.user)));
+      });
+  };
+}
+
+export const USER_CREATED = 'USER_CREATED';
+export function userCreated(user) {
+  return {
+    type: USER_CREATED,
+    user
+  };
+}
+
+export function updateUser(login, data) {
+  return dispatch => {
+    Request.patch(`/api/users/${login}`)
+      .send(data)
+      .end((err, response) => {
+        if (err != null) {
+          console.error(err); // TODO: Add ui error handling
+        }
+
+        response = normalize(JSON.parse(response.text), userSchema);
+        dispatch(userUpdated(Immutable.fromJS(response.entities.user)));
+      });
+  };
+}
+
+export const USER_UPDATED = 'USER_CREATED';
+export function userUpdated(user) {
+  return {
+    type: USER_UPDATED,
+    user
+  };
+}
+
+export function deleteUser(login) {
+  return dispatch => {
+    Request.del(`/api/users/${login}`)
+      .end((err, response) => {
+        if (err != null) {
+          console.error(err); // TODO: Add ui error handling
+        }
+
+        dispatch(userDeleted(login));
+      });
+  };
+}
+
+export const USER_DELETED = 'USER_DELETED';
+export function userDeleted(login) {
+  return {
+    type: USER_DELETED,
+    login
   };
 }
