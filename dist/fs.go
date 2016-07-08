67b233d05b260225414a86d4b3de1fd354da9f37
Brad Rydzewski <brad.rydzewski@gmail.com>
2016-07-08 13:17:57 -0700

    added fs for embedded app

---
+++ dist/fs.go
@@ -0,0 +1,10 @@
+package dist
+
+import "github.com/elazarl/go-bindata-assetfs"
+
+func AssetFS() *assetfs.AssetFS {
+	for k := range _bintree.Children {
+		return &assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: k}
+	}
+	panic("unreachable")
+}
