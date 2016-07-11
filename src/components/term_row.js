a2d87b06be32c7173a004f945485597fe54632ca
Brad Rydzewski <brad.rydzewski@gmail.com>
2016-07-11 11:16:30 -0700

    initial work to add line numbers, issue #29

--- src/components/term_row.js
+++ src/components/term_row.js
@@ -8,14 +8,18 @@ const opts = {use_classes: true}
 export default
 class TermRow extends React.Component {
   render() {
-    const {pos, out} = this.props.line;
+    const {pos, out, time} = this.props.line;
 
     let html = ansi_up.escape_for_html(out || '');
     html = ansi_up.ansi_to_html(html, opts);
     html = ansi_up.linkify(html);
 
     return (
-     <div className="term-row" dangerouslySetInnerHTML={{__html: html}}></div>
+     <div className="term-row">
+      <div>{pos+1}</div>
+      <div dangerouslySetInnerHTML={{__html: html}}></div>
+      <div>{time || 0}s</div>
+     </div>
     );
   }
 }
