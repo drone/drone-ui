52c3bb38db8262c08b707a16e054847e2cbc9ac4
Gilles De Mey <gilles.de.mey@gmail.com>
2016-10-01 20:40:30 +0200

    Makes linenumbers and time indicator non-selectable, fixes #54 As an added bonus it aligns line-numbers to the right (up to 9.999)

--- src/components/term_row.js
+++ src/components/term_row.js
@@ -24,9 +24,9 @@ class TermRow extends React.Component {
 
     return (
      <div className="term-row">
-      <div>{pos+1}</div>
-      <div dangerouslySetInnerHTML={{__html: html}}></div>
-      <div>{time || 0}s</div>
+      <div className="row-line-number">{pos+1}</div>
+      <div className="row-output" dangerouslySetInnerHTML={{__html: html}}></div>
+      <div className="row-time">{time || 0}s</div>
      </div>
     );
   }
