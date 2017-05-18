08c40b03e941d2da3a063d58c156f99a84065d40
Marco Vito Moscaritolo <mavimo@gmail.com>
2017-05-18 19:27:39 +0200

    refs #115: fix build duration

--- src/components/build_panel.js
+++ src/components/build_panel.js
@@ -56,8 +56,8 @@ class BuildPanel extends React.Component {
           <div>
             <div>
               <i className="material-icons">access_time</i>
-              {job.started_at ?
-                <TimeAgo date={(job.started_at || build.created_at) * 1000} /> :
+              {job.end_time ?
+                <Humanize finished={job.end_time} start={job.start_time} /> :
                 <span>--</span>
               }
             </div>
