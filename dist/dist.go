8bf182c6469f3da5a5cfb9cf10e43ddefe7be13e
Brad Rydzewski <brad.rydzewski@gmail.com>
2017-08-02 16:48:24 -0400

    fetch polyfills from CDN

--- dist/dist.go
+++ dist/dist.go
@@ -1,3 +1,3 @@
 package dist
 
-//go:generate togo http -package dist -output dist_gen.go -input ../build/default/** -trim-prefix "../build/default"
+//go:generate togo http -package dist -output dist_gen.go -input ../build/default/** -trim-prefix "../build/default" --exclude "bower_components/webcomponentsjs/(.+)"
