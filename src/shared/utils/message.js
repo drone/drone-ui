c27f57af068ec7d8fbbcc608787515e9260dffdb
Lanre Adelowo <me@lanreadelowo.com>
2018-04-01 00:45:12 +0100

    fixed non disappering notifications

--- src/shared/utils/message.js
+++ src/shared/utils/message.js
@@ -1,11 +1,15 @@
 /**
- * Displays the globa message.
+ * Displays the global message.
  *
  * @param {Object} tree - The drone state tree.
  * @param {string} message - The message text.
  */
 export const displayMessage = (tree, message) => {
 	tree.set(["message", "text"], message);
+
+	setTimeout(() => {
+		hideMessage(tree);
+	}, 5000);
 };
 
 /**
