194c2fa0f6fb7f7f96cff248b3c6e1110eb50bc3
Brad Rydzewski <brad.rydzewski@gmail.com>
2018-11-09 07:27:32 -0800

    page transition

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -5,7 +5,9 @@
     </template>
 
     <template>
-      <router-view />
+      <transition name="fadein" mode="out-in">
+        <router-view/>
+      </transition>
     </template>
   </base-layout>
 </template>
@@ -23,3 +25,12 @@ export default {
   computed: {}
 };
 </script>
+
+<style scoped>
+.fadein-enter-active, .fadein-leave-active {
+  transition: opacity .125s;
+}
+.fadein-enter, .fadein-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
