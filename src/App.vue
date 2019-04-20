3d397c6a8fcaf10d565f6db3299ecfa19da3089e
Alex Barashkov <lnikell@gmail.com>
2019-04-20 12:33:25 +0200

    Fixed #270, removed importing of Google fonts, since we moved to system fonts

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Roboto+Mono:300,400,500');
 @import "./assets/styles/reset.scss";
 @import "./assets/styles/global.scss";
 </style>
