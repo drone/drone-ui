5b0a004b9634cb261924545ac728f3c9df770c54
Rewanth Cool <rewanth1997@gmail.com>
2019-07-28 03:29:27 -0700

    Integrates text-security npm module to Secrets component Prevents shoulder surfing

--- src/components/editable-list/Secrets.vue
+++ src/components/editable-list/Secrets.vue
@@ -17,7 +17,7 @@
 
     <template slot="fields">
       <BaseInput name="secret.name" v-model="secret.name" placeholder="Secret Name" type="text"/>
-      <BaseTextArea name="secret.data" v-model="secret.data" placeholder="Secret Value"/>
+      <BaseTextArea name="secret.data" v-model="secret.data" class="hide_secret" placeholder="Secret Value"/>
       <BaseCheckbox v-model="secret.pullRequest" style="margin-bottom: 12px;">Allow Pull Requests</BaseCheckbox>
     </template>
   </EditableList>
@@ -86,6 +86,11 @@ export default {
 </script>
 
 <style scoped>
+@import "../../../node_modules/text-security/dist/text-security-disc.css";
+.hide_secret {
+    font-family: 'text-security-disc';
+}
+
 .icon-secrets-empty {
   display: block;
   margin: 0 auto 20px;
