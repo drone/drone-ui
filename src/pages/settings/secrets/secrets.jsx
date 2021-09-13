faa9b80824e07a2726f6eaa1da9a83c87e30f77f
Dan Wilson <daniel.wilson@harness.io>
2021-09-13 11:32:04 +0100

    add org secrets to repo settings page

--- src/pages/settings/secrets/secrets.jsx
+++ src/pages/settings/secrets/secrets.jsx
@@ -1,92 +1,18 @@
 import classNames from 'classnames/bind';
-import PropTypes from 'prop-types';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Button from 'components/shared/button';
-import Form, { Field, FormSection } from 'components/shared/form';
 import Modal, { useModal } from 'components/shared/modal';
 import { useToast } from 'hooks';
 import { useSecrets } from 'hooks/swr';
 import { ReactComponent as DemoIcon } from 'svg/demo.svg';
-import { ReactComponent as TrashIcon } from 'svg/trash.svg';
 import { axiosWrapper } from 'utils';
 
 import styles from './secrets.module.scss';
+import { NewSecretForm, SecretListView } from './shared';
 
 const cx = classNames.bind(styles);
-
-const NewSecretForm = ({ handleSubmit, handleCancel }) => {
-  const [state, setState] = useState({
-    name: '',
-    data: '',
-    pull_request: false,
-  });
-  const handleSecretChange = (field) => (event) => {
-    switch (field) {
-      case 'name':
-      case 'data':
-        setState((prev) => ({ ...prev, [field]: event.target.value }));
-        break;
-      case 'pull_request':
-        setState((prev) => ({ ...prev, pull_request: event.target.checked }));
-        break;
-      default:
-    }
-  };
-  const handleAddSecret = (e) => {
-    handleSubmit(state);
-    handleCancel();
-  };
-  return (
-    <Form
-      className={cx('container')}
-    >
-      <FormSection className={cx('group', 'group-fields')}>
-        <Field.Input
-          placeholder="Secret Name"
-          value={state.name}
-          label="Name"
-          name="secret-name"
-          width={350}
-          autoFocus="true"
-          onChange={handleSecretChange('name')}
-        />
-        <Field.TextArea
-          label="Value"
-          placeholder="Secret value"
-          value={state.data}
-          name="secret-value"
-          width={350}
-          onChange={handleSecretChange('data')}
-        />
-        <Field.Checkbox
-          label="Allow Pull Requests"
-          name="pull_request"
-          checked={state.pull_request}
-          onChange={handleSecretChange('pull_request')}
-        />
-      </FormSection>
-      <FormSection className={cx('controls')}>
-        <Button
-          onClick={handleAddSecret}
-        >
-          Create
-        </Button>
-        <Button
-          onClick={handleCancel}
-        >
-          Cancel
-        </Button>
-      </FormSection>
-    </Form>
-  );
-};
-
-NewSecretForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
-  handleCancel: PropTypes.func.isRequired,
-};
 
 export default function Secrets() {
   const { namespace, name } = useParams();
@@ -111,7 +37,8 @@ export default function Secrets() {
   }, [mutate, namespace, name, showSuccess, showError]);
 
   const handleRemoveSecret = (secretName) => async () => {
-    const userAgreed = confirm('Are you sure you want to delete this secret?');
+    // eslint-disable-next-line no-alert
+    const userAgreed = window.confirm('Are you sure you want to delete this secret?');
     if (userAgreed) {
       try {
         await axiosWrapper(`/api/repos/${namespace}/${name}/secrets/${secretName}`, {
@@ -169,39 +96,6 @@ export default function Secrets() {
           handleCancel={toggleModal}
         />
       </Modal>
-    </>
-  );
-}
-
-function SecretListView(props) {
-  return (
-    <div className={cx('secret-list-wrapper')}>
-      <div className={cx('secret-list-header')}>
-        <div>Name</div>
-        <div>Pull Requests</div>
-        <div />
-      </div>
-      <div className={cx('secret-list')}>
-        {props?.secrets?.map((secret) => (
-          <SecretListItem data={secret} key={secret.id} onRemove={props.handleRemove} />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function SecretListItem({ data, onRemove }) {
-  return (
-    <>
-      <div className={cx('secret-list-item')}>
-        <div>{data.name}</div>
-        <div>{data.pull_request ? 'Enabled' : 'Disabled'}</div>
-        <div>
-          <Button onClick={onRemove(data.name)}>
-            <TrashIcon />
-          </Button>
-        </div>
-      </div>
     </>
   );
 }
